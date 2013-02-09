0e58068554d95730494245f1cf73d972ba4c91ee
Danilo Fiorenzano <tomade@gmail.com>
2013-02-08 19:42:32 -0500

    Done with all buttons other than outline, which shows a weird behaviour I cannot understand (see code comments for details).

--- Attribute/ATTViewController.m
+++ Attribute/ATTViewController.m
@@ -34,7 +34,68 @@
 }
 
 
+- (IBAction)redButton {
+    [self changeAttributes:@{NSForegroundColorAttributeName : [UIColor redColor]}];
+}
 
+- (IBAction)greenButton {
+    [self changeAttributes:@{NSForegroundColorAttributeName : [UIColor greenColor]}];
+}
+
+- (IBAction)blueButton {
+    [self changeAttributes:@{NSForegroundColorAttributeName : [UIColor blueColor]}];
+}
+
+- (IBAction)yellowButton {
+    [self changeAttributes:@{NSForegroundColorAttributeName : [UIColor yellowColor]}];
+}
+
+- (IBAction)changeFont:(UIButton*)sender {
+    // Take font from button, replace size with the one used in the sentence, apply
+    // resulting font to the sentence
+
+    UIFont* buttonFont = [[sender titleLabel] font];
+    UIFont* sentenceFont = [self.sentence.attributedText attribute:NSFontAttributeName
+                                                           atIndex:0
+                                                    effectiveRange:NULL];
+    UIFont* newFont = [buttonFont fontWithSize:[sentenceFont pointSize]];
+    [self changeAttributes:@{NSFontAttributeName:newFont}];
+    
+}
+
+
+- (IBAction)underline {
+    [self changeAttributes: @{NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle)} ];
+}
+
+
+- (IBAction)noUnderline {
+    [self changeAttributes: @{NSUnderlineStyleAttributeName: @(NSUnderlineStyleNone)} ];
+}
+
+- (IBAction)outline {
+
+    // Don't understand what's going on here!  The foreground white is obscuring the stroke,
+    // but only if the stroke colour is black!
+    // Also, why changing the fg colour with the buttons loses the stroke attributes??
+
+
+    [self changeAttributes:
+     @{
+NSStrokeWidthAttributeName: @(-3.0),
+NSStrokeColorAttributeName: [UIColor blackColor],
+NSForegroundColorAttributeName: [UIColor whiteColor]
+     }];
+}
+
+
+
+- (void) changeAttributes:(NSDictionary *) attrs {
+    NSMutableAttributedString *newString = [self.sentence.attributedText mutableCopy];
+    NSRange where = [[newString string] rangeOfString: self.selectedWord.text];
+    [newString addAttributes:attrs range:where];
+    self.sentence.attributedText = newString;
+}
 
 
 
